9d9ba4f080c671d2d9ef698657c887710089bf1c
Cory Cantey <corycantey@gmail.com>
2025-10-02 21:39:48 -0400

    Print more timing info

--- src/train.rs
+++ src/train.rs
@@ -6,7 +6,7 @@ use clap::Parser;
 use pgn_reader::{BufferedReader, SanPlus, Visitor};
 use rayon::prelude::*;
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::PathBuf; use std::time::Instant;
 
 use chessers::mlp::Mlp;
 use chessers::{board_to_tensor, move_to_index};
@@ -75,13 +75,17 @@ fn main() -> Result<()> {
     // Set the OPENBLAS_NUM_THREADS environment variable to 1.
     // This allows Rayon to manage the parallelism at a higher level.
     // See: https://github.com/huggingface/candle/issues/213
-    std::env::set_var("OPENBLAS_NUM_THREADS", "1");
+    // std::env::set_var("OPENBLAS_NUM_THREADS", "1");
 
     let device = Device::cuda_if_available(0).unwrap_or(Device::Cpu);
     println!("Using device: {:?}", device);
 
     // 1. Initialize model and optimizer
-    let varmap = VarMap::new();
+    let mut varmap = VarMap::new();
+    if std::path::PathBuf::from(&args.output_file).exists() {
+        println!("Loading model from {:?}", args.output_file);
+        varmap.load(&args.output_file)?;
+    }
     let vb = VarBuilder::from_varmap(&varmap, DType::F32, &device);
     let model = Mlp::new(vb)?;
     let mut optimizer = AdamW::new(model.vars(), ParamsAdamW { lr: args.learning_rate , beta1: 0.9, beta2: 0.999, eps: 1e-8, weight_decay: 0.0 })?;
@@ -91,7 +95,8 @@ fn main() -> Result<()> {
         args.epochs, args.learning_rate
     );
 
-    const GAMES_PER_BATCH: usize = 32;
+    const BATCH_SIZE: usize = 1024 * 32; // Number of positions per batch
+    const EPOCH_SIZE: usize = 16; // Number of batches per epoch
 
     // 2. Training loop
     for epoch in 0..args.epochs {
@@ -101,75 +106,79 @@ fn main() -> Result<()> {
 
         let mut total_epoch_loss = 0.0;
         let mut total_moves_in_epoch = 0;
-        let mut batch_count = 0;
+        let mut batch_num = 0;
 
         loop {
             // --- Data Collection Phase ---
-            let mut games_data = Vec::with_capacity(GAMES_PER_BATCH);
-            for _ in 0..GAMES_PER_BATCH {
-                let mut batch_data = vec![];
-                for _ in 0..GAMES_PER_BATCH {
-                    let mut collector = GameDataCollector::new();
-                    match reader.read_game(&mut collector)? {
-                        Some(Ok(game_positions)) if !game_positions.is_empty() => {
-                            batch_data.extend(game_positions);
-                        }
-                        Some(_) => {} // Skip empty or failed games
-                        None => break, // End of file
+            let data_loading_start = Instant::now();
+            let mut batch_positions = Vec::with_capacity(BATCH_SIZE);
+            let mut eof = false;
+            while batch_positions.len() < BATCH_SIZE {
+                let mut collector = GameDataCollector::new();
+                match reader.read_game(&mut collector)? {
+                    Some(Ok(game_positions)) if !game_positions.is_empty() => {
+                        batch_positions.extend(game_positions);
                     }
+                    Some(_) => {} // Skip empty or failed games
+                    None => {
+                        eof = true;
+                        break;
+                    } // End of file
                 }
-                games_data.push(batch_data)
+            }
+            let data_loading_duration = data_loading_start.elapsed();
+
+            if batch_positions.is_empty() {
+                break; // No more positions to process
             }
 
-            if games_data.is_empty() {
-                break; // No more games in the file
-            } else {
-                println!("Read {} games", games_data.len());
-            }
-
-            let moves_in_batch = games_data.len();
+            let moves_in_batch = batch_positions.len();
             total_moves_in_epoch += moves_in_batch;
-            batch_count += 1;
+            batch_num += 1;
 
             // --- Parallel Processing and Gradient Accumulation ---
-            let batch_losses: Vec<Tensor> = games_data
+            let tensor_conv_start = Instant::now();
+            let (board_tensors, target_indices): (Vec<_>, Vec<_>) = batch_positions
                 .par_iter()
-                .map(|batch| {
-                    batch
-                    .iter()
-                    .map(|(board, chess_move)| {
-                        let board_tensor = board_to_tensor(board, &device)?;
-                        let logits = model.forward(&board_tensor)?; // Shape [4096]
-                        Ok(logits.unsqueeze(0)) // Shape [1, 4096]
-                    })
-                    .collect::<Result<Vec<_>>>()
+                .map(|(board, chess_move)| {
+                    let board_tensor = board_to_tensor(board, &device).expect("Failed to convert board to tensor");
+                    let move_index = move_to_index(*chess_move) as u32;
+                    (board_tensor, move_index)
                 })
-                .collect::<Result<Vec<Vec<_>>>>()?
-                .into_iter()
-                .flatten()
-                .collect::<Result<Vec<_>, candle_core::Error>>()?;
+                .unzip();
+            let tensor_conv_duration = tensor_conv_start.elapsed();
 
-            // Create targets tensor
-            let targets: Vec<u32> = games_data
-                .iter()
-                .flatten()
-                .map(|(_, chess_move)| move_to_index(*chess_move) as u32)
-                .collect();
-            let targets_tensor = Tensor::new(targets.as_slice(), &device)?;
+            let forward_pass_start = Instant::now();
+            let input_tensor = Tensor::stack(&board_tensors, 0)?;
+            let logits = model.forward(&input_tensor)?;
+            let forward_pass_duration = forward_pass_start.elapsed();
 
-            // Calculate loss
-            let logits_tensor = Tensor::cat(&batch_losses, 0)?;
-            let total_loss = loss::cross_entropy(&logits_tensor, &targets_tensor)?;
+            let loss_start = Instant::now();
+            let targets_tensor = Tensor::new(target_indices.as_slice(), &device)?;
+            let total_loss = loss::cross_entropy(&logits, &targets_tensor)?;
+            let loss_duration = loss_start.elapsed();
+
+            let loss_backward_start = Instant::now();
             optimizer.backward_step(&total_loss)?;
+            let loss_backward_duration = loss_backward_start.elapsed();
 
-            println!("Saving progress to {:?}", args.output_file);
-            varmap.save(&args.output_file)?;
+            println!(
+                "Batch {:<5} | Positions: {:<4} | Data: {:<10?} | Tensors: {:<10?} | Forward: {:<10?} | Loss: {:<10?} | Backward: {:<10?}",
+                batch_num, moves_in_batch, data_loading_duration, tensor_conv_duration, forward_pass_duration, loss_duration, loss_backward_duration
+            );
 
             total_epoch_loss += total_loss.to_scalar::<f32>()?;
+
+            if eof || batch_num >= EPOCH_SIZE {
+                break;
+            }
         }
 
         let avg_loss = total_epoch_loss / total_moves_in_epoch as f32;
-        println!( "Epoch: {:4} | Batches: {:5} | Avg Loss: {:8.5}", epoch + 1, batch_count, avg_loss);
+        println!( "Epoch: {:4} | Batches: {:5} | Avg Loss: {:8.5}", epoch + 1, batch_num, avg_loss);
+
+        println!("Saving model after epoch {} to {:?}", epoch + 1, args.output_file);
+        varmap.save(&args.output_file)?;
     }
 
     println!("Training complete. Saving model to {:?}", args.output_file);
